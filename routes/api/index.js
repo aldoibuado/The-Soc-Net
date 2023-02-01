82c27c2a8441632802220cf23502db86e98c47cc
aldoibuado <aldo.ibuado@gmail.com>
2023-02-01 13:44:01 -0800

    added index.js in regards to api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => res.send('Wrong route!'));
+router.use('/users', userRoutes);
 
 module.exports = router;
